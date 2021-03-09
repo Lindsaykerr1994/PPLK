243ae56195bb06f63c3b67ae247d503c5f61f124
Lindsaykerr1994 <lindsaykerr1994@gmail.com>
2021-03-09 08:42:56 +0000

    Changed line-gradient background to more complex 3 line gradient

---
+++ pplk/src/Components/Welcome/Subcomponents/CrestText.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Crest(){
+    return(
+        <div id="portal">
+            <p>&#60;&#62;</p>
+        </div>
+    )
+}
--- pplk/src/Components/Welcome/Welcome.js
+++ pplk/src/Components/Welcome/Welcome.js
@@ -2,19 +2,17 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import ColourBar from '../Navbar/Subcomponents/ColourBar';
 import SocialBar from '../Navbar/Subcomponents/SocialBar';
-import Crest from './Subcomponents/SimplifiedCrest';
+import Crest from './Subcomponents/CrestText';
 import '../../Assets/Welcome.css';
 import '../../Assets/WelcomeBackground.css';
 
 export default function Welcome() {
     return(
         <div id="welcome-container" className="main-container">
-            <h4>Lindsay Kerr - Web Development</h4>
             <NavLink to="/biography">
-                <div id="portal">
-                    <Crest />
-                </div>
+                <Crest />
             </NavLink>
+            <h4>Lindsay Kerr - Web Development</h4>
             <SocialBar />
             <ColourBar />
         </div>
